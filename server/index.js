3d3609629b0f2167ef7db1bb6c35710d8254b67d
Chad Basinger <chad.basingeris@gmail.com>
2021-03-24 21:23:09 -0600

    step 2

---
+++ server/index.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const express = require('express')
+const massive = require('massive')
+const session = require('express-session')
+const app = express()
+const authCtrl = require('./controllers/authController')
+
+app.use(express.json())
+
+const {PORT, CONNECTION_STRING, SESSION_SECRET} = process.env
+
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: true,
+    saveUninitialized: false
+}))
+
+app.post('/auth/register', authCtrl.register)
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {
+        rejectUnauthorized: false
+    }
+})
+.then(dbInstance => {
+    app.set('db', dbInstance)
+    app.listen(PORT, () => console.log(`Server is riding port ${PORT}`))
+})
+.catch(err => console.log(err))
+
+
